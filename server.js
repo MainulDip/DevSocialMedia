e836570b0a9e75b6c258292ec68720048b8bc7ea
Mainul Dip <mainuldip@gmail.com>
2019-07-23 05:57:26 +0600

    Include npm install on package json

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
 
 // Serve Static Assets In Production Mode
-if (process.env.NODE_ENV === 'productin') {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
